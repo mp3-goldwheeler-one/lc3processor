2d0dd00b0f1d43e4dfe7837abeaa9b6bb8e0ea8f
Jeff Wheeler <wheele11@illinois.edu>
2012-03-07 20:04:32 -0600

    Start IF

--- hdl/id_reg_pipe_untitled.vhd
+++ hdl/id_reg_pipe_untitled.vhd
@@ -14,24 +14,24 @@ USE ieee.NUMERIC_STD.all;
 LIBRARY ece411;
 USE ece411.LC3b_types.all;
 
-ENTITY decode_reg_pipe IS
+ENTITY decode_exec_pipe IS
    PORT( 
-      CLK             : IN     std_logic;
-      RESET_L         : IN     std_logic;
-      control_word    : IN     LC3b_word;
-      decode_pc       : IN     LC3b_word;
-      opcode          : IN     LC3b_opcode;
-      reg_controlword : OUT    LC3b_word;
-      reg_opcode      : OUT    LC3b_word;
-      reg_pc          : OUT    LC3b_word
+      CLK              : IN     std_logic;
+      RESET_L          : IN     std_logic;
+      control_word     : IN     LC3b_word;
+      decode_pc        : IN     LC3b_word;
+      opcode           : IN     LC3b_opcode;
+      exec_controlword : OUT    LC3b_word;
+      exec_opcode      : OUT    LC3b_word;
+      exec_pc          : OUT    LC3b_word
    );
 
 -- Declarations
 
-END decode_reg_pipe ;
+END decode_exec_pipe ;
 
 --
-ARCHITECTURE untitled OF decode_reg_pipe IS
+ARCHITECTURE untitled OF decode_exec_pipe IS
 BEGIN
 END ARCHITECTURE untitled;
 
