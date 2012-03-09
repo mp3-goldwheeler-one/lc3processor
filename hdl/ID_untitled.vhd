3431efed620226b5ed582c14ab201c5f6fcd90fe
Jeff Wheeler <wheele11@illinois.edu>
2012-03-08 19:20:12 -0600

    Start doing decode and a few more pipes

---
+++ hdl/ID_untitled.vhd
@@ -0,0 +1,56 @@
+--
+-- VHDL Architecture ece411.IR.untitled
+--
+-- Created:
+--          by - goldste6.UNKNOWN (linux4.ews.illinois.edu)
+--          at - 20:25:39 01/18/12
+--
+-- using Mentor Graphics HDL Designer(TM) 2005.3 (Build 75)
+--
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+USE ieee.NUMERIC_STD.all;
+
+LIBRARY ece411;
+USE ece411.LC3b_types.all;
+
+ENTITY ID IS
+   PORT( 
+      decode_instr : IN     LC3b_word;
+      Opcode       : OUT    LC3b_opcode;
+      SrcA         : OUT    LC3b_reg;
+      SrcB         : OUT    LC3b_reg;
+      dest         : OUT    LC3b_reg;
+      index6       : OUT    LC3b_index6;
+      imm5         : OUT    LC3b_imm5;
+      bit5         : OUT    std_logic;
+      offset9      : OUT    LC3b_offset9;
+      trapvec8     : OUT    LC3b_trapvect8;
+      offset11     : OUT    LC3b_offset11;
+      bit11        : OUT    std_logic;
+      imm4         : OUT    LC3b_imm4;
+      direction    : OUT    std_logic
+   );
+
+-- Declarations
+
+END ID ;
+
+--
+ARCHITECTURE UNTITLED OF ID IS
+SIGNAL VAL_IR : LC3B_WORD;
+BEGIN
+	OPCODE    <= decode_instr(15 DOWNTO 12);
+	SRCA      <= decode_instr(8 DOWNTO 6);
+	SRCB      <= decode_instr(2 DOWNTO 0);
+	DEST      <= decode_instr(11 DOWNTO 9);
+	OFFSET9   <= decode_instr(8 DOWNTO 0);
+	INDEX6    <= decode_instr(5 DOWNTO 0);
+	imm5      <= decode_instr(4 downto 0);
+	imm4      <= decode_instr(3 downto 0);
+	offset11  <= decode_instr(10 downto 0);
+	trapvec8  <= decode_instr(7 downto 0);
+	bit5      <= decode_instr(5);
+	direction <= decode_instr(4);
+	bit11     <= decode_instr(11);
+END UNTITLED;
--- hdl/id_reg_pipe_untitled.vhd
+++ hdl/id_reg_pipe_untitled.vhd
@@ -16,23 +16,23 @@ USE ece411.LC3b_types.all;
 
 ENTITY decode_exec_pipe IS
    PORT( 
-      CLK              : IN     std_logic;
-      RESET_L          : IN     STD_LOGIC;
-      control_word     : IN     LC3b_word;
-      decode_instr     : IN     STD_LOGIC_VECTOR (15 DOWNTO 0);
-      decode_pc        : IN     LC3b_word;
-      opcode           : IN     LC3b_opcode;
-      exec_controlword : OUT    LC3b_word;
-      exec_opcode      : OUT    LC3b_word;
-      exec_pc          : OUT    LC3b_word
+      CLK             : IN     std_logic;
+      RESET_L         : IN     STD_LOGIC;
+      decode_instr    : IN     LC3b_word;
+      decode_opcode   : IN     LC3b_opcode;
+      decode_pc       : IN     LC3b_word;
+      decode_ready    : IN     std_logic;
+      decode_regwrite : IN     std_logic;
+      decode_src_a    : IN     LC3b_word;
+      decode_src_b    : IN     LC3b_word;
+      exec_instr      : OUT    LC3b_word;
+      exec_opcode     : OUT    LC3b_opcode;
+      exec_pc         : OUT    LC3b_word;
+      exec_regwrite   : OUT    STD_LOGIC;
+      exec_src_a      : OUT    LC3b_word;
+      exec_src_b      : OUT    LC3b_word
    );
 
 -- Declarations
 
 END decode_exec_pipe ;
-
---
-ARCHITECTURE untitled OF decode_exec_pipe IS
-BEGIN
-END ARCHITECTURE untitled;
-
