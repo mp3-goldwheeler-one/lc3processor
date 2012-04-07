f29cfc3c3798e7ce2d113de9f9fb6a469d357be6
David Goldstein <dgoldstein0@gmail.com>
2012-04-06 21:08:19 -0500

    beginning flushing

--- hdl/id_reg_pipe_untitled.vhd
+++ hdl/id_reg_pipe_untitled.vhd
@@ -22,8 +22,8 @@ ENTITY decode_exec_pipe IS
       decode_data_out       : IN     pipe_data;
       decode_instr          : IN     STD_LOGIC_VECTOR (15 DOWNTO 0);
       decode_pc             : IN     LC3b_word;
+      exec_insert_bubble    : IN     std_logic;
       load_decode_exec_pipe : IN     STD_LOGIC;
-      stall_load_use_buffer : IN     std_logic;
       exec_control          : OUT    control_word;
       exec_control_in       : OUT    exec_control_word;
       exec_data_in          : OUT    pipe_data;
